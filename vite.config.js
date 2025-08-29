52790c7e502569eb16c0771550f93949d633b419
Doomsday058 <fedorov.vlad22213@mail.ru>
2025-08-29 16:01:15 +0500

    feat: deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/testcase_ToDoApp/',
   test: {
     globals: true,
     environment: 'jsdom',
